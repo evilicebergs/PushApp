45639139dc505e5c3375b3f23c24faa3c4073bf1
evilicebergs <voiced.fair-0a@icloud.com>
2024-11-21 19:10:55 +0200

    connect firebase, configure notifications, register on APNS

--- PushApp/PushAppApp.swift
+++ PushApp/PushAppApp.swift
@@ -6,12 +6,52 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import UserNotificationsUI
+import FirebaseMessaging
+
+class AppDelegate: NSObject, UIApplicationDelegate, MessagingDelegate, UNUserNotificationCenterDelegate  {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        Messaging.messaging().delegate = self
+        UNUserNotificationCenter.current().delegate = self
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { success, _ in
+            guard success else {
+                return
+            }
+            
+            print("Success in APNS registy")
+        }
+        application.registerForRemoteNotifications()
+        
+        return true
+    }
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        messaging.token { token, _ in
+            guard let token = token else { return }
+            
+            print("Token: \(token)")
+            
+        }
+    }
+    
+}
+
+
 
 @main
 struct PushAppApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
     }
+  }
 }
