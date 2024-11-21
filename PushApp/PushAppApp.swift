73df7694bb4a9d3ab0b82098979941a9a4894202
evilicebergs <voiced.fair-0a@icloud.com>
2024-11-21 18:37:03 +0200

    Initial Commit

---
+++ PushApp/PushAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushAppApp.swift
+//  PushApp
+//
+//  Created by Artem Golovchenko on 2024-11-21.
+//
+
+import SwiftUI
+
+@main
+struct PushAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
